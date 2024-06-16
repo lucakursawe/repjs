275d9f591e7d55128c1111a620fcbd467ac5c106
Luca Kursawe <lucakursawe@gmail.com>
2024-06-16 16:13:16 +0200

    speed insights

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -3,6 +3,7 @@ import { SessionContextProvider } from '@supabase/auth-helpers-react';
 import { supabase } from '../utils/supabase';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 function MyApp({ Component, pageProps }) {
   return (
