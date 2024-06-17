80e91d01f229b7aaa98e79ab80c151a29c71b36a
Luca Kursawe <lucakursawe@gmail.com>
2024-06-17 17:05:11 +0200

    huhuhuuuuu

--- components/Header.jsx
+++ components/Header.jsx
@@ -87,7 +87,7 @@ export default function Header() {
       )}
       {/* Pop-Out Menu */}
       <div
-        className={`fixed top-0 right-0 h-full w-64 bg-primary-orange text-white transform ${
+        className={`fixed top-0 right-0 h-full w-32 bg-primary-orange text-white transform ${
           menuOpen ? 'translate-x-0' : 'translate-x-full'
         } transition-transform duration-300 ease-in-out z-20`}
       >
